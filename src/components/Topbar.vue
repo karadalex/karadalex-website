7fa3c858853c718c3b480a40412d6548a4141f64
karadalex <karadalex@gmail.com>
2019-04-14 22:27:13 +0300

    switch to classic view - topbar right element

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="os-topbar">
-    
+    <div class="os-topbar-left"></div>
+    <div class="os-topbar-right">
+      <div class="os-topbar-right-el">
+        <router-link to="/home">Switch to classic view</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,6 +26,26 @@ export default {
   background-color: #d8d8d8;
   box-shadow: 0px 0px 10px rgba(49, 49, 49, 0.582);
   border-bottom: 1px solid #a1a1a1;
+}
+
+.os-topbar-left {
+  text-align: left;
+}
+
+.os-topbar-right {
+  text-align: right;
+}
+
+.os-topbar-left-el, .os-topbar-right-el {
+  display: inline-block;
+  & a {
+    color: black;
+    text-decoration: none;
+  }
+}
+
+.os-topbar-right-el {
+  margin-right: 20px;
 }
 </style>
 
