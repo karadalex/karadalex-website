87098c44cbfe85938a908edfbccfe058c0932399
karadalex <karadalex@gmail.com>
2019-04-14 20:48:12 +0300

    Added some initial styling

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="contact">
-    <h1>Contact</h1>
+    <div class="background"></div>
+    <div class="header-container">
+      <h1>Contact</h1>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  height: 50vh;
+  // 020 New Life - https://webgradients.com/
+  background-image: linear-gradient(to right, #43e97b 0%, #38f9d7 100%);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="background"></div>
+    <div class="header-container">
+      <h1>Home</h1>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  height: 50vh;
+  // 019 Malibu Beach - https://webgradients.com/
+  background-image: linear-gradient(to right, #4facfe 0%, #00f2fe 100%);
+}
+</style>
