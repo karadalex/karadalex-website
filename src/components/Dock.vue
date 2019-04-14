8baf406fd86ac3339844f0e80de1014a366fd60f
karadalex <karadalex@gmail.com>
2019-04-14 19:44:47 +0300

    Added some Dock icons

--- src/components/Dock.vue
+++ src/components/Dock.vue
@@ -1,45 +1,66 @@
 <template>
   <div class="os-dock">
-
+    <div class="os-dock-bg"></div>
+    <div class="os-dock-icons">
+      <div class="os-dock-icon">
+        <img src="../assets/file-manager.svg"/>
+        <div class="os-dock-icon-text">Finder</div>
+      </div>
+      <div class="os-dock-icon">
+        <img src="../assets/preferences-system.svg"/>
+        <div class="os-dock-icon-text">Preferences</div>
+      </div>
+      <div class="os-dock-icon">
+        <img src="../assets/terminal.svg"/>
+        <div class="os-dock-icon-text">Terminal</div>
+      </div>
+      <div class="os-dock-icon">
+        <img src="../assets/xpad.svg"/>
+        <div class="os-dock-icon-text">Notes</div>
+      </div>
+      <div class="os-dock-icon">
+        <img src="../assets/calculator.svg"/>
+        <div class="os-dock-icon-text">Calculator</div>
+      </div>
+      <div class="os-dock-icon">
+        <img src="../assets/web-browser.svg"/>
+        <div class="os-dock-icon-text">Browser</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Dock'
+  name: 'Dock',
+
+  data() {
+    return {
+      icons: ["file-manager.svg"],
+      openedApps: []
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-// .os-dock {
-//   position: absolute;
-//   bottom: 0;
-//   left: 25vw;
-//   right: 25vw;
-//   height: 70px;
-//   // background-color: #cccccc;
-//   border-radius: 5px;
-//   background: inherit;
-// }
-
-// .os-dock:before{
-//   content: “ ”;
-//   background: inherit; 
-//   position: absolute;
-//   left: 25vw;
-//   right: 25vw;
-//   bottom: 0;
-//   box-shadow: inset 0 0 0 200px rgba(255,255,255,0.2);
-//   filter: blur(10px);
-// }
-
-.os-dock{
+.os-dock {
   width: 50vw;
   background: inherit;
   position: absolute;
   bottom: 0;
   left: 25vw;
   right: 25vw;
+  height: 70px;
+}
+
+.os-dock-bg{
+  width: 50vw;
+  background: inherit;
+  position: absolute;
+  bottom: 0;
+  // left: 25vw;
+  // right: 25vw;
   height: 70px;
   overflow: hidden;
   // margin-left: -175px;
@@ -49,7 +70,7 @@ export default {
   box-shadow: 0px 0px 10px rgba(49, 49, 49, 0.582);
 }
 
-.os-dock:before{
+.os-dock-bg:before{
   width: 60vw;
   height: 500px;
   content: "";
@@ -63,6 +84,34 @@ export default {
   filter: blur(10px);
 }
 
+.os-dock-icons {
+  text-align: left;
+  position: absolute;
+  right: 0;
+  left: 0;
+}
+
+.os-dock-icon {
+  text-align: center;
+  margin-left: 20px;
+  margin-top: 3px;
+  width: 60px;
+  display: inline-block;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  & img {
+    width: 50px;
+  }
+
+  & .os-dock-icon-text {
+    font-size: 12px;
+    font-weight: 600;
+    margin-top: -4px;
+  }
+}
 </style>
 
 
