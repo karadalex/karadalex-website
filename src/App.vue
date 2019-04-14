a58aab3e8590f7cdc3df4ae2e938830a379e82c8
karadalex <karadalex@gmail.com>
2019-04-14 20:46:55 +0300

    Bootstrap navbar

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,52 @@
 <template>
   <div id="app">
 		<template v-if="$route.name !== 'computer'">
-			<div id="nav">
-				<router-link to="/home">Home</router-link> |
-				<router-link to="/about">About</router-link> |
-				<router-link to="/contact">Contact</router-link> |
-				<router-link to="/blog">Blog</router-link> |
-				<router-link to="/portfolio">Portfolio</router-link> |
-				<router-link to="/cv">Curriculum Vitae</router-link> |
-				<router-link to="/press">Press</router-link> |
-				<router-link to="/">Switch View</router-link>
-			</div>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/home">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contact">Contact</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/blog">Blog</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/research">Research</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/portfolio">Portfolio</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/cv">Curriculum Vitae</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/press">Press</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Switch View</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 		</template>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
