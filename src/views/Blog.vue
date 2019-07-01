70cbda5802129b00283d46f85145f14ddd8347f8
karadalex <karadalex@gmail.com>
2019-07-01 23:09:13 +0300

    render blog post previews

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,12 +4,38 @@
     <div class="header-container">
       <h1>Blog</h1>
     </div>
+
+    <div class="container post-preview" v-for="(post, key) in posts" :key="key">
+      <div class="row"><h2>{{post.title}}</h2></div>
+      <div class="row">{{post.date}} | {{post.readtime}} min read</div>
+      <div class="row">
+        <p>{{post.excerpt}}</p>
+      </div>
+      <div class="row"><a href="http://">read more ...</a></div>
+    </div>
   </div>
 </template>
+
+
+<script>
+import Posts from '../content/posts.json'
+
+export default {
+  data: () => ({
+    posts: Posts
+  }),
+}
+</script>
+
+
 
 <style lang="scss" scoped>
 .background {
   // 079 Summer Games - https://webgradients.com/
   background-image: linear-gradient(to right, #92fe9d 0%, #00c9ff 100%);
+}
+
+.post-preview {
+  margin-top: 30px;
 }
 </style>
