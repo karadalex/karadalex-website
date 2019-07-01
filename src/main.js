a8534d934907d910f67e4165422fef5fa28ec6e9
karadalex <karadalex@gmail.com>
2019-07-01 20:33:01 +0300

    installed vue-material

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import { MdCard } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+// import 'vue-material/dist/theme/default.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+
+Vue.use(MdCard)
 
 Vue.config.productionTip = false
 
