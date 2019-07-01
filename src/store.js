385ead7315f9840d602bde43c42e40e2f1b0263c
karadalex <karadalex@gmail.com>
2019-07-01 20:51:58 +0300

    make config globally available

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import config from './config'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    config: config
   },
   mutations: {
 
