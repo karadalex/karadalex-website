090f09755362c30423863fcd1d68e97461eaaa8c
karadalex <karadalex@gmail.com>
2019-07-01 23:09:50 +0300

    render contact cards for email github and linkedin

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,12 +4,84 @@
     <div class="header-container">
       <h1>Contact</h1>
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <md-card class="md-elevation-10" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title"> Email</div>
+              </md-card-header>
+              <md-card-content>
+                {{ email }}
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+        </div>
+
+        <div class="col-4">
+          <md-card class="md-elevation-10" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title"> Github</div>
+              </md-card-header>
+              <md-card-content>
+                {{ github }}
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+        </div>
+        <div class="col-4">
+          <md-card class="md-elevation-10" md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <div class="md-title"> LinkedIn</div>
+              </md-card-header>
+              <md-card-content>
+                {{ linkedin }}
+              </md-card-content>
+            </md-ripple>
+          </md-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+import axios from 'axios'
+import { mapState } from 'vuex'
+
+export default {
+  methods: {
+
+  },
+
+  data: () => ({
+    repos: null
+  }),
+
+  computed: mapState({
+    github: state => state.config.githubUser,
+    email: state => state.config.email,
+    linkedin: state => state.config.linkedin,
+  }),
+
+  created() {
+    
+  }
+}
+</script>
+
 
 <style lang="scss" scoped>
 .background {
   // 041 Happy Acid - https://webgradients.com/
   background-image: linear-gradient(to top, #37ecba 0%, #72afd3 100%);
+}
+
+.md-card {
+  height: 120px;
 }
 </style>
