a5527ba4a6d2f03b3d68a08d4ee6ab939b2d1cf6
karadalex <karadalex@gmail.com>
2019-07-02 16:23:53 +0300

    conditional rendering of research projects/papers

--- src/views/Research.vue
+++ src/views/Research.vue
@@ -4,8 +4,93 @@
     <div class="header-container">
       <h1>Research</h1>
     </div>
+
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <div class="input-group-text">
+              <input v-model="publishedEnabled" type="checkbox">
+            </div>
+          </div>
+          <div class="input-group-append">
+            <span class="input-group-text">Published papers</span>
+          </div>
+
+          <div class="input-group-prepend">
+            <div class="input-group-text">
+              <input v-model="researchgateEnabled" type="checkbox">
+            </div>
+          </div>
+          <div class="input-group-append">
+            <span class="input-group-text">ResearchGate papers</span>
+          </div>
+        </div>
+      </div>
+
+      <template v-if="publishedEnabled">
+        <div class="row">
+          <h2>
+            Published papers
+          </h2>
+        </div>
+        <div class="row">
+          <!-- <div class="col-4" v-for="(repo, key) in repos" :key="key">
+            <md-card class="md-elevation-10" md-with-hover>
+              <md-ripple>
+                <md-card-header>
+                  <div class="md-title">{{ repo.name }}</div>
+                </md-card-header>
+
+                <md-card-content>
+                  {{ repo.description }}
+                </md-card-content>
+              </md-ripple>
+            </md-card>
+          </div> -->
+        </div>
+      </template>
+
+      <template v-if="researchgateEnabled">
+        <div class="row">
+          <h2>
+            ResearchGate papers
+          </h2>
+        </div>
+        <div class="row">
+          <!-- <div class="col-4" v-for="(repo, key) in repos" :key="key">
+            <md-card class="md-elevation-10" md-with-hover>
+              <md-ripple>
+                <md-card-header>
+                  <div class="md-title">{{ repo.name }}</div>
+                </md-card-header>
+
+                <md-card-content>
+                  {{ repo.description }}
+                </md-card-content>
+              </md-ripple>
+            </md-card>
+          </div> -->
+        </div>
+      </template>
+    </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  methods: {
+
+  },
+
+  data: () => ({
+    publishedEnabled: true,
+    researchgateEnabled: true
+  })
+}
+</script>
+
 
 <style lang="scss" scoped>
 .background {
